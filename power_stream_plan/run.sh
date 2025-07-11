bc3454714c065cf42a7a8aec12c191a07068dfcb
Filip Richter <filip.richter@atrea.cz>
2025-07-11 13:39:17 +0200

    cleaning

--- power_stream_plan/run.sh
+++ power_stream_plan/run.sh
@@ -5,7 +5,7 @@ echo "Starting PowerStreamPlan optimizer with Gunicorn..."
 export HASSIO_TOKEN=$HASSIO_TOKEN
 export HA_URL="http://homeassistant:8123"
 export HA_ADDON="power_stream_plan"
-export HA_ADDON_DATA="/data"
+export HA_ADDON_DATA="./data"
 
 # Spusť aplikaci pomocí Gunicorn
 gunicorn --config gunicorn.conf.py powerplan_server:app
--- power_stream_plan/run_local.sh
+++ power_stream_plan/run_local.sh
@@ -6,7 +6,7 @@ export HASSIO_TOKEN=$HASSIO_TOKEN
 export HA_URL="http://homeassistant:8123"
 # Pro testování místně nepoužíváme HA_ADDON, aby se data ukládala lokálně
 # export HA_ADDON="power_stream_plan"
-export HA_ADDON_DATA="."
+export HA_ADDON_DATA="./data"
 
 # Spusť aplikaci pomocí Gunicorn
 gunicorn --config gunicorn.conf.py powerplan_server:app
